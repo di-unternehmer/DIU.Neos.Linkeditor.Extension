c02dcbe5e074f4f595761eb4ffe527a7f8150e0b
Alexander Kappler <alexander.kappler@di-unternehmer.com>
2019-07-25 17:32:48 +0200

    WIP: Update Linkprocessor

--- Classes/Fusion/LinkProcessorImplementation.php
+++ Classes/Fusion/LinkProcessorImplementation.php
@@ -1,11 +1,27 @@
 <?php
 namespace DIU\Neos\LinkEditor\Fusion;
 
+use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Flow\Annotations as Flow;
 use Neos\Fusion\FusionObjects\AbstractFusionObject;
+use Neos\Neos\Fusion\Helper\LinkHelper;
+use Neos\Eel\FlowQuery\FlowQuery;
+use Neos\ContentRepository\Domain\Service\ContextFactory;
 
 class LinkProcessorImplementation extends AbstractFusionObject
 {
+    /**
+     * @Flow\Inject
+     * @var LinkHelper
+     */
+    protected $linkingHelper;
+
+    /**
+     * @Flow\Inject
+     * @var ContextFactory
+     */
+    protected $contextFactory;
+
     /**
      * @return string
      * @throws \Exception
@@ -22,14 +38,44 @@ class LinkProcessorImplementation extends AbstractFusionObject
             throw new \Exception(sprintf('Only strings can be processed by this Fusion object, given: "%s".', gettype($text)), 1382624080);
         }
 
+
         return preg_replace_callback(
             "/(<a.+?href=\")(.+?)\"/i",
             function($m) {
+                $nodeContext = $this->fusionValue('node');
                 $href = $m[2];
-                $href = str_replace(array("://", ".", "/", "#"), "_", $href);
-                return $m[1] . $m[2] . '" data-enuripathsegment="' . $href . '" ';
+
+                xdebug_break();
+                $originNode = $this->linkingHelper->convertUriToObject($href,$nodeContext);
+                if ($originNode) {
+                    //Create an english context
+                    $enContext = $this->contextFactory->create([
+                        'dimensions' => [
+                            'language' => ['en'],
+                            'country' => ['int']
+                        ],
+                        'targetDimensions' => [
+                            'language' => 'en',
+                            'country' => 'int'
+                        ]
+                    ]);
+
+                    $query = new FlowQuery([$enContext->getCurrentSiteNode()]);
+                    /** @var NodeInterface $enNode */
+                    $enNode = $query->find($originNode)->get(0);
+                    $enUriPathSegment = $enNode->getProperty('uriPathSegment');
+                }
+
+
+
+                if (!is_string($enUriPathSegment)) {
+                    throw new \Exception(sprintf('Only strings can be processed by this Fusion object, given: "%s".', gettype($enUriPathSegment)), 1382624080);
+                }
+
+                return $m[1] . $m[2] . '" data-enuripathsegment="' . $enUriPathSegment . '" ';
             },
             $text
         );
+
     }
 }
